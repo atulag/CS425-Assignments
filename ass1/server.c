f97d516b374d3dc5bd39da2257bd18ddc23aa9fa
Atul Agarwal <atulagarwal104@gmail.com>
2011-08-09 22:39:39 +0530

    server socket is initialized

--- ass1/server.c
+++ ass1/server.c
@@ -42,6 +42,19 @@ int main(int argc, char **argv)
     socklen_t clilen;
     struct sockaddr_in serv_addr, cli_addr;
     
+    if (argc < 2)
+        error("ERROR No port provided... exiting");
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+        error("ERROR opening socket... exiting");
+    bzero((char *) &serv_addr, sizeof(sockaddr_in));
+    portno = atoi(argv[1]);
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(portno);
+    if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
+        error("ERROR on binding socket... exiting");
+
     int shm_id1,shm_id2;
     client *cli_list;
     int *numcli;                        /* Number of online client */
@@ -54,4 +67,8 @@ int main(int argc, char **argv)
     *numcli = 0;
 
     char buffer[256];
+
+    listen(sockfd, 10);
+    clilen = sizeof(cli_addr);
+   
 }
