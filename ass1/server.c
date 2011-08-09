b75853521f599689bb0e517b24af05f2e3d4f17a
Atul Agarwal <atulagarwal104@gmail.com>
2011-08-09 21:44:21 +0530

    Client struct defined

---
+++ ass1/server.c
@@ -0,0 +1,28 @@
+/* Server File */
+
+/* Header files are included here */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/shm.h>
+#include <sys/stat.h>
+#include <netinet/in.h>
+#define MAX_Player 10
+
+/* Structure to store online client information */
+
+typedef struct
+{
+    short in_game;
+    short in_use;
+    char login_name[50];
+    char ip_addr[20];
+    unsigned short portno;
+    short request;
+}client;
+
+
