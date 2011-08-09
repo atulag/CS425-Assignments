8ea452916b39a2b9617a97aa51ccea5ec4359b24
Atul Agarwal <atulagarwal104@gmail.com>
2011-08-09 22:29:04 +0530

    shared memory initialized

--- ass1/server.c
+++ ass1/server.c
@@ -33,3 +33,25 @@ void error(const char *msg)
     exit(1);
 }
 
+/*Main function starts here */
+
+int main(int argc, char **argv)
+{
+    int sockfd, newsockfd, portno;
+    pid_t pid;
+    socklen_t clilen;
+    struct sockaddr_in serv_addr, cli_addr;
+    
+    int shm_id1,shm_id2;
+    client *cli_list;
+    int *numcli;                        /* Number of online client */
+    const int size = MAX_Player*sizeof(client);
+    shm_id1 = shmget(IPC_PRIVATE, size, S_IRUSR | S_IWUSR);
+    shm_id2 = shmget(IPC_PRIVATE, sizeof(int), S_IRUSR | S_IWUSR);
+    cli_list = (client *) shmat(shm_id1, NULL, 0);
+    numcli = (int *) shmat(shm_id2, NULL, 0);
+    memset(cli_list, 0, size);
+    *numcli = 0;
+
+    char buffer[256];
+}
